ae97eafbd9ab783eee262895fb0f45ed6d35625a
Vlad <VIStrelnikov94@gmail.com>
2023-01-27 23:46:40 +0300

    Add new procedure AddDoctor

---
+++ RecordingSystem.DB/dbo/Stored Procedures/AddDoctor.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE [dbo].[AddDoctor]
+	@Name nvarchar(20),
+	@LastName nvarchar(30),
+	@Male bit,
+	@PhoneNumber nvarchar(10),
+	@Email nvarchar(20),
+	@SpecializationId int,
+	@CabinetId int,
+	@TimeTableId int,
+	@Birthday date
+AS
+INSERT INTO dbo.[Doctor]
+(
+	Name,
+	LastName,
+	Male,
+	PhoneNumber,
+	Email,
+	SpecializationId,
+	CabinetId,
+	TimeTableId,
+	Birthday 
+)
+VALUES
+(
+	@LastName,
+	@Male,
+	@PhoneNumber,
+	@Email,
+	@SpecializationId,
+	@CabinetId,
+	@TimeTableId,
+	@Birthday
+)
