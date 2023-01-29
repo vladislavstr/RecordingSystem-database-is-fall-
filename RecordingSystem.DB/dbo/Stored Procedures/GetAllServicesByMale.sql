fabe819be6fdcbfefb06156cce5b5c524f596015
Vlad <VIStrelnikov94@gmail.com>
2023-01-29 20:56:28 +0300

    Add prcedure GettAllServicesbyMalle

---
+++ RecordingSystem.DB/dbo/Stored Procedures/GetAllServicesByMale.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[GetAllServicesByMale]
+@Male bit
+AS
+SELECT Sp.Id, Sp.Name As SpecializationName, Se.Name AS ServiceName, Se.Price
+FROM Service AS Se
+INNER JOIN [dbo].Specialization AS Sp ON Se.SpecializationId = Sp.Id
+WHERE @Male = Se.Male
