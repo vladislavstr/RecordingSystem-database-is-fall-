ae1c84b070b27bfa624e7c37a0c3f8535c8d1171
Svyatoslav <svat.strel.2001@gmail.com>
2023-01-29 14:43:13 +0300

    Change DoctorRepository

--- RecordingSystem.DAL/DoctorRepository.cs
+++ RecordingSystem.DAL/DoctorRepository.cs
@@ -9,12 +9,13 @@ namespace RecordingSystem.DAL
     public class DoctorRepository
     {
         public List<DoctorDto> GetAllDoctors()
-        { 
-            var sqlConnection = new SqlConnection(Options.sqlConnection);
-            sqlConnection.Open();
-            var result = sqlConnection.Query<DoctorDto>(StoredProcedures.GetAllDoctors, commandType: CommandType.StoredProcedure).ToList();
-            sqlConnection.Close();
-            return result;
+        {
+            using (var sqlConnection = new SqlConnection(Options.sqlConnection))
+            {
+                sqlConnection.Open();
+                return sqlConnection.Query<DoctorDto>(StoredProcedures.GetAllDoctors, 
+                    commandType: CommandType.StoredProcedure).ToList();
+            }
         }
     }
 }
